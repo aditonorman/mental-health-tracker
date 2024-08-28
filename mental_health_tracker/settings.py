64bbea9c773ba28cc3942100f39e4a52216fc7db
aditonorman <muhammad.raditya33@ui.ac.id>
2024-08-28 12:08:00 +0700

    Initial commit

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7l=r*c58&%qpmy3nllpuy#rj=qf=^_q4mww$+oh92)-zjt9@pe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","muhammad-raditya33-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
